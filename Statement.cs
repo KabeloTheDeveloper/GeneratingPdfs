690cade21a2034ed5408b9b116e58595f5ddbbd4
KabeloTheDeveloper <117746545+KabeloTheDeveloper@users.noreply.github.com>
2022-11-24 16:21:38 +0200

    Add files via upload

---
+++ Statement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pdfs.Moldels
+{
+    public class Statement
+    {
+        public Company Customer { get; set; }
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public DateTime StatementDate { get; set; } = DateTime.Now;
+        public int statementPageNumber { get; set; } = 1;
+        public decimal Current { get; set; } = 0.0M;
+        public decimal ThirtyDays { get; set; } = 0.0M;
+        public decimal SixtyDays { get; set; } = 0.0M;
+        public decimal NinetyDays { get; set; } = 0.0M;
+        public decimal OneTwentyDays { get; set; } = 0.0M;
+        public decimal Total { get; set; } = 0.0M;
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pdfs.Moldels
+{
+    public class Transaction
+    {
+        public string AccNumber { get; set; } = "";
+        public string Date { get; set; } = "";
+        public string RaNumber { get; set; } = "";
+        public string Voucher { get; set; } = "";
+        public string OrderNumebr { get; set; } = "";
+        public string DriverName { get; set; } = "";
+        public string Details { get; set; } = "";
+        public string Debit { get; set; } = "";
+        public string Credit { get; set; } = "";
+        public string Amount { get; set; } = "";
+    }
+}
